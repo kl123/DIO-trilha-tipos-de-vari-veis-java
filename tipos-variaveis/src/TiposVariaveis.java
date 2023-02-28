03deeece9852d1307e707df97d4c92b5a392a2ea
Kletion Paulino da Silva <kleitonpaulinodasilva@gmail.com>
2023-02-28 14:59:48 -0300

    primeira modificacao

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,9 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int numero = 10;
+        final double VALOR_PI = 3.14;
+        System.out.println(VALOR_PI + numero);
+
     }
 }
