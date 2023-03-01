34641d5b80a8f2b3a911ff863affcde1903a0a36
Kleiton Paulino da Silva <Kleitonpaulinodasilva@gmail.com>
2023-03-01 12:14:30 -0300

    Delete tipos-variaveis directory

--- tipos-variaveis/src/TiposVariaveis.java
+++
@@ -1,9 +0,0 @@
-public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        int numero = 10;
-        final double VALOR_PI = 3.14;
-        System.out.println(VALOR_PI + numero);
-
-    }
-}
